ce3ad8c9d5cdbcfcc6297506b716ed7cd52ba41f
and1713 <a.jhoanjimenez1726@gmail.com>
2024-11-22 11:39:45 +0100

    primer commit

---
+++ andrea.js
@@ -0,0 +1,61 @@
+let saludo = " Holaaa";
+
+let despedida;
+despedida="Adios";
+saludo = "hola";
+let nombreusuario = "Andrea";
+
+//alert(saludo);
+
+
+const SALUDO_USUARIO = "hola";
+
+
+
+
+
+
+console.log(SALUDO_USUARIO);
+console.log("hola que tal");
+
+console.log(5>4);// mayor que 
+console.log(5<8);//menor que 
+
+console.log(5>=6);//mayor o igual 
+console.log(5<=7);//menor o igaul 
+
+console.log(5==5);//igual que
+console.log(5!=7);// no es igual 
+
+console.log( "a">"b"); 
+
+let edad = 15;
+
+if( edad>=18) { 
+    alert( " eres mayor de edad ");
+}
+
+let premium = true;
+
+if( premium ) { 
+
+    console.log( " tienes cuenta premium");//si es premium 
+
+}else if( edad>18){ 
+console.log ( " puedes una cuenta ");//si no es premium, pero es +18
+
+}else{ 
+    console.log("no puedes crear una cuenta ");
+}
+
+//interatuar con html Don 
+document.getElementById("texto").innerHTML="Holaaaaa";
+//document.getElementsByClassName8("articulo")
+document.getElementById("texto2").innerHTML = "holaaaaaaaaaaaaa";
+
+
+
+
+
+
+
